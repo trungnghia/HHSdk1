45d8a20b8a2703addd04d596425ea1abb029ae67
mac <mac2@macs-MacBook-Pro.local>
2016-12-01 14:19:13 +0700

    ban 3.0

---
+++ HHSdk.framework/Headers/HHSdk.h
@@ -0,0 +1,19 @@
+//
+//  HHSdk.h
+//  HHSdk
+//
+//  Created by mac on 11/11/16.
+//  Copyright © 2016 Hoàng Hiệp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <HHSdk/HoangHiep.h>
+//! Project version number for HHSdk.
+FOUNDATION_EXPORT double HHSdkVersionNumber;
+
+//! Project version string for HHSdk.
+FOUNDATION_EXPORT const unsigned char HHSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HHSdk/PublicHeader.h>
+
+
